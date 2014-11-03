702e03464a5ff24f766f0ad0d31234987975a08e
Ahmad <Ahmad@194.47.44.26>
2014-11-03 11:50:41 +0100

    skrev

---
+++ JavaApplication78/src/javaapplication78/JavaApplication78.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication78;
+
+/**
+ *
+ * @author Ahmad
+ */
+public class JavaApplication78 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        while(true){
+            
+        }
+    }
+}
